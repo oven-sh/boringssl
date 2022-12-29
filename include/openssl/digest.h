23941efd0dddda2ae759849aa87a0c54be60db1b
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-12-28 21:50:47 -0800

    Apply patches from https://github.com/electron/electron/tree/main/patches/boringssl

--- include/openssl/digest.h
+++ include/openssl/digest.h
@@ -90,6 +90,9 @@ OPENSSL_EXPORT const EVP_MD *EVP_blake2b256(void);
 // MD5 and SHA-1, as used in TLS 1.1 and below.
 OPENSSL_EXPORT const EVP_MD *EVP_md5_sha1(void);
 
+// EVP_ripemd160 is in decrepit and not available by default.
+OPENSSL_EXPORT const EVP_MD *EVP_ripemd160(void);
+
 // EVP_get_digestbynid returns an |EVP_MD| for the given NID, or NULL if no
 // such digest is known.
 OPENSSL_EXPORT const EVP_MD *EVP_get_digestbynid(int nid);
